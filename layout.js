57df4fb94b4b608c4e7e9a6631f869d6a79d0636
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-10-17 16:49:14 +0100

    basic html layout function

---
+++ layout.js
@@ -0,0 +1,18 @@
+function layout(title, content) {
+	return /* html */ `
+        <!DOCTYPE html>
+        <html lang="en">
+          <head>
+              <meta charset="utf-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1">
+              <title>${title}</title>
+          </head>
+
+          <body>
+            ${content}
+          </body>
+        </html>
+    `;
+}
+
+module.exports = layout;
